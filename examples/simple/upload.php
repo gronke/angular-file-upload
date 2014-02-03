ab243b5cd776424222cade63b338eeb7341494b9
Stefan Grönke <stefan@gronke.net>
2014-02-03 21:15:44 +0100

    allow file uploads to subdirectory only

--- examples/simple/upload.php
+++ examples/simple/upload.php
@@ -1,10 +1,17 @@
 <?php
 
+$dir = realpath(dirname( __FILE__ ) . '/uploads');
+
 if ( !empty( $_FILES ) ) {
 
     $tempPath = $_FILES[ 'file' ][ 'tmp_name' ];
-    $uploadPath = dirname( __FILE__ ) . '\uploads\\' . $_FILES[ 'file' ][ 'name' ];
+    $uploadPath = $dir . '/' . $_FILES[ 'file' ][ 'name' ];
 
+    if(strpos($uploadPath, $dir) === -1) {
+        header("HTTP/1.0 400 Bad Request");
+        exit;
+    }
+    
     move_uploaded_file( $tempPath, $uploadPath );
 
     $answer = array( 'answer' => 'File transfer completed' );
